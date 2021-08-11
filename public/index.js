7d17b2df5a7dac96e9bce2adb6c35809fd856a4b
Dylan Wiseman <dylan.wiseman22@gmail.com>
2021-08-11 16:11:07 -0600

    displaying results

--- public/index.js
+++ public/index.js
@@ -1,29 +1,23 @@
-// const { default: axios } = require("axios");
-
 const { adzunaID, adzunaKey } = adzunaConfig;
 const URLs = {
   adzunaBaseURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&`,
   adzunaURLEnd: `&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
-  adzunaWebDevURL: `${this.adzunaBaseURL}what_phrase=web%20developer${this.adzunaURLEnd}`,
-  adzunaJavascriptURL: `${this.adzunaBaseURL}what=javascript${this.adzunaURLEnd}`,
-  adzunaPythonURL: `${this.adzunaBaseURL}what=python${this.adzunaURLEnd}`,
-  adzunaJavaURL: `${this.adzunaBaseURL}what=Java${this.adzunaURLEnd}`,
-  adzunaFullStackURL: `${this.adzunaBaseURL}what_phrase=full%20stack${this.adzunaURLEnd}`,
-  adzunaFrontEndURL: `${this.adzunaBaseURL}what=frontend${this.adzunaURLEnd}`,
-  adzunaBackEndURL: `${this.adzunaBaseURL}what=backend${this.adzunaURLEnd}`,
-  adzunaSoftwareEngineerURL: `${this.adzunaBaseURL}what_phrase=software%20engineer${this.adzunaURLEnd}`,
+  adzunaWebDevURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what_phrase=web%20developer&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaJavascriptURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what=javascript&what_exclude=senior%20manager%20sr%20principal%20java%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaPythonURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what=python&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaJavaURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what=Java&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaFullStackURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what_phrase=full%20stack&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaFrontEndURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what=frontend&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaBackEndURL: `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what=backend&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
+  adzunaSoftwareEngineerURL: `$https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=${adzunaID}&app_key=${adzunaKey}&results_per_page=50&what_phrase=software%20engineer&what_exclude=senior%20manager%20sr%20principal%20&max_days_old=30&salary_include_unknown=1&full_time=1&permanent=1`,
 };
+let container = document.getElementById("job-list");
 
 const showWebDevJobs = () => {
-  console.log("working");
   axios
     .get(`${URLs.adzunaWebDevURL}`)
     .then((res) => {
-      console.log("retrieved");
-      res.data.results.map((job) => {
-        console.log(job.title, job.company.display_name);
-      });
-      //   console.log(res.data.results);
+      createJobCards(res);
     })
     .catch((err) => console.log("Error: " + err));
 };
@@ -32,11 +26,33 @@ const showJavascriptJobs = () => {
   axios
     .get(`${URLs.adzunaJavascriptURL}`)
     .then((res) => {
-      res.data.results.map((job) => {
-        console.log(job.title, job.company.display_name);
-      });
+      createJobCards(res);
     })
     .catch((err) => console.log("Error: " + err));
+};
+
+const createJobCards = (res) => {
+  container.innerHTML = "";
+  console.log("createJobsFunction!");
+  res.data.results.map((job) => {
+    let jobCard = document.createElement("div");
+    let jobTitle = document.createElement("h3");
+    let company = document.createElement("h4");
+    let description = document.createElement("p");
+    let link = document.createElement("a");
+    jobCard.classList.add("job-card");
+    jobTitle.textContent = job.title;
+    company.textContent = job.company.display_name;
+    description.textContent = job.description;
+    link.href = job.redirect_url;
+    link.textContent = "link";
+    // console.log(jobTitle.textContent, company.textContent);
+    container.appendChild(jobCard);
+    jobCard.appendChild(jobTitle);
+    jobCard.appendChild(company);
+    jobCard.appendChild(description);
+    jobCard.appendChild(link);
+  });
 };
 
 // Tried using async/await:
